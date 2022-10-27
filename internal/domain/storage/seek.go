d350ae5a3f89aed48ca6b550eb22ca35dd2aa920
mel2oo <l977631253@gmail.com>
2022-10-27 23:10:32 +0800

    add storage

---
+++ internal/domain/storage/seek.go
@@ -0,0 +1,119 @@
+package storage
+
+import (
+	"bazaar/pkg/fileutil"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+const (
+	_StartNo = 1
+	_MaxSize = 2
+)
+
+type Seeks struct {
+	rootpath string
+	seekfile string
+	seekinfo map[string]Info
+}
+
+type Info struct {
+	path string
+	size int
+}
+
+func InitSeek(rootpath, seekfile string) (*Seeks, error) {
+	skinfo := make(map[string]Info)
+	skpath := filepath.Join(rootpath, seekfile)
+
+	if fileutil.Exist(skpath) {
+		data, err := os.ReadFile(skpath)
+		if err != nil {
+			return nil, err
+		}
+
+		current := make(map[string]string)
+		if err := json.Unmarshal(data, &current); err != nil {
+			return nil, err
+		}
+
+		for ext, path := range current {
+			size, err := fileutil.FileSize(path)
+			if err != nil {
+				return nil, err
+			}
+			skinfo[ext] = Info{
+				path: path,
+				size: size,
+			}
+		}
+	}
+
+	return &Seeks{
+		rootpath: rootpath,
+		seekfile: seekfile,
+		seekinfo: skinfo,
+	}, nil
+}
+
+func (s *Seeks) Check(root, ext string) (string, error) {
+	var newpath string
+
+	info, ok := s.seekinfo[ext]
+	if ok {
+		if info.size < _MaxSize {
+			s.seekinfo[ext] = Info{
+				path: info.path,
+				size: info.size + 1,
+			}
+			return info.path, nil
+		}
+
+		// 如果达到单目录存储上限, 则切换目录
+		_, sno := filepath.Split(info.path)
+		no, err := strconv.Atoi(sno)
+		if err != nil {
+			return info.path, err
+		}
+		newpath = filepath.Join(root, ext, fmt.Sprintf("%03d", no+1))
+	}
+
+	// 无同类型样本目录则首次创建
+	if !ok {
+		newpath = filepath.Join(root, ext, fmt.Sprintf("%03d", _StartNo))
+	}
+
+	if err := os.MkdirAll(newpath, os.ModePerm); err != nil {
+		return newpath, err
+	}
+
+	s.seekinfo[ext] = Info{
+		path: newpath,
+		size: 0,
+	}
+	return newpath, s.Save()
+}
+
+func (s *Seeks) Save() error {
+	current := make(map[string]string)
+	for ext, info := range s.seekinfo {
+		current[ext] = info.path
+	}
+
+	data, err := json.Marshal(&current)
+	if err != nil {
+		return err
+	}
+
+	fi, err := os.Create(filepath.Join(s.rootpath, s.seekfile))
+	if err != nil {
+		return err
+	}
+	defer fi.Close()
+
+	_, err = fi.Write(data)
+	return err
+}
---
+++ internal/domain/storage/storage.go
@@ -0,0 +1,70 @@
+package storage
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+var (
+	ErrPathInvalid = errors.New("root path invalid")
+)
+
+// Config represents the storage config.
+type Config struct {
+	Root string `mapstructure:"root-path"`
+	Seek string `mapstructure:"seek-file"`
+}
+
+type Client struct {
+	root  string
+	seeks *Seeks
+	mutex *sync.Mutex
+}
+
+func New(c *Config) (*Client, error) {
+	stat, err := os.Stat(c.Root)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if err := os.Mkdir(c.Root, os.ModePerm); err != nil {
+				return nil, err
+			}
+		}
+	} else {
+		if !stat.IsDir() {
+			return nil, ErrPathInvalid
+		}
+	}
+
+	seeks, err := InitSeek(c.Root, c.Seek)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		root:  c.Root,
+		seeks: seeks,
+		mutex: new(sync.Mutex),
+	}, nil
+}
+
+func (c *Client) Create(data []byte, md5, ext string) (string, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	path, err := c.seeks.Check(c.root, ext)
+	if err != nil {
+		return path, err
+	}
+
+	full := filepath.Join(path, md5)
+	file, err := os.Create(full)
+	if err != nil {
+		return full, err
+	}
+	defer file.Close()
+
+	_, err = file.Write(data)
+	return full, err
+}
